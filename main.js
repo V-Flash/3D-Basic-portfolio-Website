060d6d311cb4f60f4462c02ca4a22bc99b333de4
Vivek Parmar <58722335+V-Flash@users.noreply.github.com>
2024-01-12 11:20:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -48,12 +48,12 @@ Array(200).fill().forEach(addStar);
 
 //Main Background 
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('/space.jpg');
 scene.background = spaceTexture;
 
 
 //Avatar
-const avatarTexture = new THREE.TextureLoader().load('profile.jpg');
+const avatarTexture = new THREE.TextureLoader().load('/profile.jpg');
 
 const avatar = new THREE.Mesh(
     new THREE.BoxGeometry(3,3,3),
@@ -64,7 +64,7 @@ scene.add(avatar)
 
 //Earth
 
-const earthTexture = new THREE.TextureLoader().load('earth_nightmap.jpg');
+const earthTexture = new THREE.TextureLoader().load('/earth_nightmap.jpg');
 
 
 const earth = new THREE.Mesh(
@@ -85,8 +85,8 @@ avatar.position.x = 2;
 
 //Mars
 
-const marsTexture = new THREE.TextureLoader().load('mars.jpg');
-//const marsBumpTexture = new THREE.TextureLoader().load('bump.jpg');
+const marsTexture = new THREE.TextureLoader().load('/mars.jpg');
+
 
 const mars = new THREE.Mesh(
     new THREE.SphereGeometry(3,35,35),
